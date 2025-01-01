49ff92a2e7d1909168116be1bff901bf2fd23394
Jessica Moore <jessica.cmoore1@gmail.com>
2025-01-01 17:21:43 -0500

    :wrench: Configure fonts and colors from style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          orange: "hsl(25, 97%, 53%)",
+        },
+        body: {
+          400: "hsl(217, 12%, 63%)",
+          700: "hsl(213, 19%, 18%)",
+          900: "hsl(216, 12%, 8%)",
+        },
+      },
+      fontFamily: {
+        sans: ['"Overpass"', "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
